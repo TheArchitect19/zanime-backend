7fc3799ac18af58e4056ad78e6f4bb6e3759c181
Ayush <ayush.krsh9@gmail.com>
2023-08-27 00:21:29 +0530

    micro-cors added

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
 //IMPORTS
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import multer from 'multer';
 import path from 'path';
 import dotenv from 'dotenv';
+import microCors from 'micro-cors'; // Import micro-cors
 
 import UploadRoute from './routes/UploadRoutes.js';
 
 dotenv.config();
 
-
-
-
-//MIDDLEWARES
+// MIDDLEWARES
 const app = express();
 
-
-const corsOptions = {
-  origin: 'https://zanime-frontend.vercel.app', // Allow requests from all domains
+const cors = microCors({
+  origin: 'https://zanime-frontend.vercel.app', // Allow requests from the specified domain
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
   credentials: true, // Allow credentials (cookies, authorization headers)
-};
+});
 
-
-
-app.use(cors(corsOptions));
+app.use(cors); // Use micro-cors as middleware
 app.use(express.json());
 app.use(express.static('public'));
 
-
-//ROUTES
+// ROUTES
 app.use(UploadRoute);
 
-
-const PORT = process.env.PORT ;
-
-//DATABASE CONNECTION
+// DATABASE CONNECTION
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
 
 // Export the Express API
 export default app;
